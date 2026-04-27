88b9b625a98429538d09d4d4fbb9ac27dbf3f950
Kausar <kausarqayoom3@gmail.com>
2026-04-27 07:41:37 +0530

    Upgraded the app

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-def add(a,b):
-   return a + b 
-print("hello bmw")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error"
+    return a / b
 
--- test_app.py
+++ test_app.py
@@ -1,3 +1,16 @@
-from app import add
+from app import add, subtract, multiply, divide
+
 def test_add():
-   assert add(5, 3) == 8
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 3) == 2
+
+def test_divide_by_zero():
+    assert divide(5, 0) == "Error"
