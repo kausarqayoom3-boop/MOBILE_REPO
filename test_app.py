66586e2d76f9c1311be18c5c020def2ef89ab2f5
Kausar <kausarqayoom3@gmail.com>
2026-04-29 08:21:28 +0530

    Added Linting

--- test_app.py
+++ test_app.py
@@ -1,16 +1,21 @@
 from app import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(5, 3) == 2
 
+
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert multiply(2, 3) == 8
+
 
 def test_divide():
     assert divide(6, 3) == 2
+
 
 def test_divide_by_zero():
     assert divide(5, 0) == "Error"
